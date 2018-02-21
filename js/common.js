eda7048bfdcf447fa83c06200533fca1f0d91e34
Margaret <atiragram77@gmail.com>
2018-02-21 12:06:56 +0300

    dest

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(window).on('load', function () {
+    var $preloader = $('#p_prldr'),
+        $svg_anm   = $preloader.find('.svg_anm');
+    $svg_anm.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+});
